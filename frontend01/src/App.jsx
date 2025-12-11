4c9acfc28bfcf3030f17833b520eed2ed34f2b46
Your Name <your-email@example.com>
2025-12-11 08:51:55 +0545

    updated frontend

--- frontend01/src/App.jsx
+++ frontend01/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
